bc350d9737628ef631f0e11e63c12099d176b812
hulklee1 <hulklee@gmail.com>
2024-10-02 16:52:42 +0900

    3

--- Test01/Test01/main.c
+++ Test01/Test01/main.c
@@ -1,20 +1,13 @@
-/*
- * Test01.c
- *
- * Created: 2024-10-02 오후 12:17:17
- * Author : user
- */ 
-
-#include <avr/io.h>
-
-
+#include <avr/io.h>  // AVR 기본 헤더
+// LED Port : PG4
 int main(void)
 {
     /* Replace with your application code */
-	printf("Hello World!");
+	//printf("Hello World!");
+	DDRG |= 0x10;  // xxxx xxxx ==> xxx1 xxxx: 1=출력  0=입력 
     while (1) 
     {
-		
+		PORTG |= 0x10;
     }
 }
 
